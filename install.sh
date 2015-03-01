44ee8a0af9497ed623062787cf5603431da9c564
Jay Chand <jchand5066@gmail.com>
2015-03-01 18:40:08 -0500

    Added install.sh file to get required dependency files

---
+++ install.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+cd cookbooks
+
+git clone git@github.com:opscode-cookbooks/xml.git 
+git clone git@github.com:sethvargo/chef-sugar.git
+git clone git@github.com:opscode-cookbooks/chef_handler.git 
+git clone git@github.com:opscode-cookbooks/dmg.git 
+git clone git@github.com:hw-cookbooks/runit.git 
+git clone git@github.com:hw-cookbooks/windows.git 
+git clone git@github.com:hw-cookbooks/yum.git 
+git clone git@github.com:chef-cookbooks/yum-epel.git 
